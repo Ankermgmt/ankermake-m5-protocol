3e0d5a31bf14768846cefb223d362a390cc22ab5
Christian Iversen <ci@iversenit.dk>
2023-03-09 20:40:22 +0100

    + Added demo program, demonstrating use of libflagship to parse pppp data

---
+++ demo-pppp.py
@@ -0,0 +1,18 @@
+import libflagship.pppp as pppp
+from rich import print
+
+# binary message to parse
+input = b'\xf1C\x00,EUPRAKM\x00\x00\x0009ABCDE\x00\x00\x00\x00\x02iz(\x1e\x14\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+print(f"input:   {input}")
+
+# parsing a message into structured data
+msg, tail = pppp.Message.parse(input)
+print(f"decoded: {msg}")
+
+# packing a structured message into binary output
+output = msg.pack()
+print(f"encoded: {output}")
+
+# the output must match our original input
+assert input == output
