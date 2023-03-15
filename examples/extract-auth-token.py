6c8b73e1fdbbb6e26c74da0a24f846326fbf8cac
secprepper <thomasjpatterson@users.noreply.github.com>
2023-03-15 16:30:30 -0400

    Enhancing the extract-auth-token.py script (#2)
    + Added the ability to have the script automatically grab Windows or Apple login.json files so the user doesn't have to.
    
    Contributed by @thomasjpatterson. Thank you!

--- examples/extract-auth-token.py
+++ examples/extract-auth-token.py
@@ -4,12 +4,41 @@ from libflagship.util import b64d
 import libflagship.logincache
 import json
 import sys
+import platform
+from os import path
+import getopt
 
-if len(sys.argv) != 2:
-    print(f"usage: {sys.argv[0]} <path/to/login.json>")
-    exit(1)
+def print_login(filename):
+    data = open(filename).read()
+    jsonj = json.loads(libflagship.logincache.decrypt(data))
+    print(jsonj["data"]["auth_token"])
 
-data = open(sys.argv[1]).read()
+def main():
+    useros = platform.system()
 
-json = json.loads(libflagship.logincache.decrypt(data))
-print(json["data"]["auth_token"])
+    darfileloc = path.expanduser('~/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json')
+    winfileloc = path.expandvars(r'%LOCALAPPDATA%\Ankermake\AnkerMake_64bit_fp\login.json')
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"haf:", ["help", "auto", "file="])
+    except getopt.GetoptError:
+      print ('extract-auth-token.py -a OR extract-auth-token.py -f <path-to-login.json>')
+      return 2
+
+    for opt, arg in opts: 
+        if opt == '-h':
+            print ('extract-auth-token.py -a OR extract-auth-token.py -f <path-to-login.json> \nIf spaces are in file location remember to escape them with a backslash')
+            return 1
+        elif opt in ("-f", "--file"):
+            print_login(arg)
+        elif opt in ("-a", "--auto"):
+            if useros == 'Darwin':
+                print_login(darfileloc)
+            elif useros == 'Windows':
+                print_login(winfileloc)
+        else:
+            print ("extract-auth-token.py -a OR extract-auth-token.py -f <path-to-login.json>")
+            return 1
+
+if __name__ == "__main__":
+    exit(main())
