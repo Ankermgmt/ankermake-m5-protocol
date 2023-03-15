b9f80487a739367ff9530cb2968b84bdc645454b
secprepper <thomasjpatterson@users.noreply.github.com>
2023-03-15 13:01:52 -0400

    Update extract-auth-token.py
    Added the ability to have the script automatically grab Windows or Apple login.json files so the user doesn't have to.

--- examples/extract-auth-token.py
+++ examples/extract-auth-token.py
@@ -4,12 +4,29 @@ from libflagship.util import b64d
 import libflagship.logincache
 import json
 import sys
+import platform
+import os
 
-if len(sys.argv) != 2:
-    print(f"usage: {sys.argv[0]} <path/to/login.json>")
+userdir = os.getlogin()
+useros = platform.system()
+
+#darfileloc = f'/Users/{userdir}/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json'
+darfileloc = f'/Users/thomaspatterson/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json'
+winfileloc = os.path.join('C:\\Users\\') + f'{userdir}' + os.path.join('\\AppData\\Local\\Ankermake\\login.json')
+
+if useros == 'Darwin':
+    data = open(f'{darfileloc}').read()
+    json = json.loads(libflagship.logincache.decrypt(data))
+    print(json["data"]["auth_token"])
     exit(1)
-
-data = open(sys.argv[1]).read()
-
-json = json.loads(libflagship.logincache.decrypt(data))
-print(json["data"]["auth_token"])
+elif useros == 'Windows':
+    data = open(f'{winfileloc}').read()
+    json = json.loads(libflagship.logincache.decrypt(data))
+    print(json["data"]["auth_token"])
+    exit(1)
+elif len(sys.argv) != 2:
+    print(f"usage: {sys.argv[0]} <path/to/login.json>")
+    data = open(sys.argv[1]).read()
+    json = json.loads(libflagship.logincache.decrypt(data))
+    print(json["data"]["auth_token"])
+    exit(1)
