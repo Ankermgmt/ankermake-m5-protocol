9454468fa990c39e1fee0ddcc8f7bfb971b2dbac
secprepper <thomasjpatterson@users.noreply.github.com>
2023-03-15 13:54:41 -0400

    Update extract-auth-token.py
    Reworked the whole damn thing to take variables from command line input

--- examples/extract-auth-token.py
+++ examples/extract-auth-token.py
@@ -6,27 +6,43 @@ import json
 import sys
 import platform
 import os
+import getopt
 
-userdir = os.getlogin()
-useros = platform.system()
+def jankauto():
+    userdir = os.getlogin()
+    useros = platform.system()
 
-#darfileloc = f'/Users/{userdir}/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json'
-darfileloc = f'/Users/thomaspatterson/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json'
-winfileloc = os.path.join('C:\\Users\\') + f'{userdir}' + os.path.join('\\AppData\\Local\\Ankermake\\login.json')
+    #darfileloc = f'/Users/{userdir}/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json'
+    darfileloc = f'/Users/thomaspatterson/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json'
+    winfileloc = os.path.join('C:\\Users\\') + f'{userdir}' + os.path.join('\\AppData\\Local\\Ankermake\\login.json')
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"haf:", ["help", "auto", "file="])
+    except getopt.GetoptError:
+      print ('extract-auth-token.py -a OR extract-auth-token.py -f <path-to-login.json>')
+      sys.exit(2)
 
-if useros == 'Darwin':
-    data = open(f'{darfileloc}').read()
-    json = json.loads(libflagship.logincache.decrypt(data))
-    print(json["data"]["auth_token"])
-    exit(1)
-elif useros == 'Windows':
-    data = open(f'{winfileloc}').read()
-    json = json.loads(libflagship.logincache.decrypt(data))
-    print(json["data"]["auth_token"])
-    exit(1)
-elif len(sys.argv) != 2:
-    print(f"usage: {sys.argv[0]} <path/to/login.json>")
-    data = open(sys.argv[1]).read()
-    json = json.loads(libflagship.logincache.decrypt(data))
-    print(json["data"]["auth_token"])
-    exit(1)
+    for opt, arg in opts: 
+        if opt == '-h':
+            print ('extract-auth-token.py -a OR extract-auth-token.py -f <path-to-login.json> \nIf spaces are in file location remember to escape them with a backslash')
+            exit(1)
+        elif opt in ("-f", "--file"):
+            data = open(arg).read()
+            jsonj = json.loads(libflagship.logincache.decrypt(data))
+            print(jsonj["data"]["auth_token"])
+            exit(1)
+        elif opt in ("-a", "--auto"):
+            if useros == 'Darwin':
+                data = open(f'{darfileloc}').read()
+                jsonj = json.loads(libflagship.logincache.decrypt(data))
+                print(jsonj["data"]["auth_token"])
+                exit(1)
+            elif useros == 'Windows':
+                data = open(f'{winfileloc}').read()
+                jsonj = json.loads(libflagship.logincache.decrypt(data))
+                print(jsonj["data"]["auth_token"])
+                exit(1)
+        else:
+            assert False, "unhandled option"
+
+jankauto()
