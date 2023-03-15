ece8eed3411d5522998ce7ad90ea122b43f0777b
Christian Iversen <ci@iversenit.dk>
2023-03-15 22:34:43 +0100

    * Normalize whitespace

--- examples/extract-auth-token.py
+++ examples/extract-auth-token.py
@@ -18,14 +18,14 @@ def main():
 
     darfileloc = path.expanduser('~/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json')
     winfileloc = path.expandvars(r'%LOCALAPPDATA%\Ankermake\AnkerMake_64bit_fp\login.json')
-    
+
     try:
         opts, args = getopt.getopt(sys.argv[1:],"haf:", ["help", "auto", "file="])
     except getopt.GetoptError:
-      print ('extract-auth-token.py -a OR extract-auth-token.py -f <path-to-login.json>')
-      return 2
+        print('extract-auth-token.py -a OR extract-auth-token.py -f <path-to-login.json>')
+        return 2
 
-    for opt, arg in opts: 
+    for opt, arg in opts:
         if opt == '-h':
             print ('extract-auth-token.py -a OR extract-auth-token.py -f <path-to-login.json> \nIf spaces are in file location remember to escape them with a backslash')
             return 1
