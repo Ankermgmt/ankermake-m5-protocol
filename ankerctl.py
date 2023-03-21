515657376ef7f4bd6116d3cf8ce6b8b870e6ccd6
Christian Iversen <ci@iversenit.dk>
2023-03-21 22:00:52 +0100

    * Rewrote "config show" command to use new config format. Also has nicer output now.

--- ankerctl.py
+++ ankerctl.py
@@ -72,17 +72,24 @@ def config_import(obj, filename):
     print(f"config_import {filename}")
 
 @config.command("show")
-@click.pass_obj
-def config_show(cfg):
+@pass_env
+def config_show(env):
     """Show current config"""
 
-    with cfg.printers() as printers:
-        if printers:
-            print("Printers:")
-            for p in printers:
-                print(f"  {p}")
-        else:
+    with env.config.open() as cfg:
+        if not cfg:
             log.error("No printers configured. Run 'config import' to populate.")
+            return
+
+        log.info("Account:")
+        print(f"    user_id: {cfg.account.user_id[:20]}...")
+        print(f"    email:   {cfg.account.email}")
+        print(f"    region:  {cfg.account.region}")
+        print()
+
+        log.info("Printers:")
+        for p in cfg.printers:
+            print(f"    {p.sn}: {p.p2p_duid}")
 
 if __name__ == "__main__":
     main()
