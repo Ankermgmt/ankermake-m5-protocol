4f8595b3c97f11eadb1bb36aaa5b85332a1ef9e0
Christian Iversen <ci@iversenit.dk>
2023-03-21 21:54:50 +0100

    * Reworked config system entirely. It now accepts a whitelist of supported class names, and serialized the type in each object-derived dictionary. This allows automatic, typed import of python objects from json.

--- cli/config.py
+++ cli/config.py
@@ -1,13 +1,19 @@
 from platformdirs import PlatformDirs
 
+from .model import Serialize, Account, Printer, Config
+
 from pathlib import Path
 import contextlib
 import json
 
 class BaseConfigManager:
 
-    def __init__(self, dirs: PlatformDirs):
+    def __init__(self, dirs: PlatformDirs, classes=None):
         self._dirs = dirs
+        if classes:
+            self._classes = {t.__name__: t for t in classes}
+        else:
+            self._classes = []
         dirs.user_config_path.mkdir(exist_ok=True)
 
     @contextlib.contextmanager
@@ -26,21 +32,43 @@ class BaseConfigManager:
     def config_path(self, name):
         return self.config_root / Path(f"{name}.json")
 
+    def _load_json(self, val):
+        if "__type__" not in val:
+            return val
+
+        typename = val["__type__"]
+        if typename not in self._classes:
+            return val
+
+        return self._classes[typename].from_dict(val)
+
+    @staticmethod
+    def _save_json(val):
+        if not isinstance(val, Serialize):
+            return val
+
+        data = val.to_dict()
+        data["__type__"] = type(val).__name__
+        return data
+
     def load(self, name, default):
         path = self.config_path(name)
         if not path.exists():
             return default
 
-        return json.load(path.open())
+        return json.load(path.open(), object_hook=self._load_json)
 
     def save(self, name, value):
         path = self.config_path(name)
-        path.write_text(json.dumps(value))
+        path.write_text(json.dumps(value, default=self._save_json, indent=2) + "\n")
 
 class AnkerConfigManager(BaseConfigManager):
 
-    def printers(self, write=False):
-        return self._borrow("printers", [], write)
+    def modify(self):
+        return self._borrow("default", write=True)
 
-def configmgr():
-    return AnkerConfigManager(PlatformDirs("ankerctl"))
+    def open(self):
+        return self._borrow("default", write=False)
+
+def configmgr(profile="default"):
+    return AnkerConfigManager(PlatformDirs("ankerctl"), classes=(Config, Account, Printer))
