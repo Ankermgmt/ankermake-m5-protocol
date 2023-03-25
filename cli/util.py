a09b966459a29fa06dcdff3a14df0e095ec427f4
Christian Iversen <ci@iversenit.dk>
2023-03-25 22:08:32 +0100

    + Implemented click argument type for handling Enums as arguments

--- cli/util.py
+++ cli/util.py
@@ -1,3 +1,5 @@
+import click
+
 def json_key_value(str):
     if not "=" in str:
         raise ValueError("Invalid 'key=value' argument")
@@ -9,3 +11,19 @@ def json_key_value(str):
             return key, float(value)
         except:
             return key, value
+
+class EnumType(click.ParamType):
+    def __init__(self, enum):
+        self.__enum = enum
+
+    def get_missing_message(self, param: "Parameter") -> str:
+        return "Choose number or name from:\n{choices}".format(choices="\n".join(f"{e.value:10}: {e.name}" for e in sorted(self.__enum)))
+
+    def convert(self, value, param, ctx):
+        try:
+            return self.__enum(int(value))
+        except:
+            try:
+                return self.__enum[value]
+            except:
+                self.fail(self.get_missing_message(param), param, ctx)
