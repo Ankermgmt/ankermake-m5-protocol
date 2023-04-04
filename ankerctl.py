bff99f05b2ce0da5ecaaf753f0f1573211b1ff76
LazeMSS <github@jemi.dk>
2023-04-04 20:08:01 +0200

    More root login.json - adding support config decode Also fixed the error message to be the same as on import

--- ankerctl.py
+++ ankerctl.py
@@ -340,14 +340,21 @@ def config_decode(env, fd):
         useros = platform.system()
 
         darfileloc = path.expanduser('~/Library/Application Support/AnkerMake/AnkerMake_64bit_fp/login.json')
-        winfileloc = path.expandvars(r'%LOCALAPPDATA%\Ankermake\AnkerMake_64bit_fp\login.json')
+        winfileloc1 = path.expandvars(r'%LOCALAPPDATA%\Ankermake\AnkerMake_64bit_fp\login.json')
+        winfileloc2 = path.expandvars(r'%LOCALAPPDATA%\Ankermake\login.json')
 
-        if useros == 'Darwin' and path.exists(darfileloc):
-            fd = open(darfileloc, 'r')
-        elif useros == 'Windows' and path.exists(winfileloc):
-            fd = open(winfileloc, 'r')
-        else:
-            exit("This platform does not support autodetection. Please specify file location")
+        try:
+            if useros == 'Darwin':
+                fd = open(darfileloc, 'r')
+            elif useros == 'Windows':
+                if path.isfile(winfileloc1):
+                    fd = open(winfileloc1, 'r')
+                else:
+                    fd = open(winfileloc2, 'r')
+            else:
+                log.critical("This platform does not support autodetection. Please specify file location")
+        except FileNotFoundError:
+            log.critical("Failed to import file - check if you are logged into Ankerslicer")
 
     log.info("Loading file..")
 
