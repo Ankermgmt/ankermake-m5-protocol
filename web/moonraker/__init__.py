eec422545a38f0415b9f86e5afe4394106e689ec
Christian Iversen <ci@iversenit.dk>
2023-04-15 18:16:31 +0200

    * Moved web/moonraker/rpc_server_database.py to web/moonraker/rpc/server/files.py

--- web/moonraker/__init__.py
+++ web/moonraker/__init__.py
@@ -1,5 +1,5 @@
 import web.moonraker.rpc_printer
 import web.moonraker.rpc_server
-import web.moonraker.rpc_server_files
 import web.moonraker.rpc_machine
 import web.moonraker.rpc.server.database
+import web.moonraker.rpc.server.files
