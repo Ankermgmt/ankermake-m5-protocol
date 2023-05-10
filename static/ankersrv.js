7611c1a38eaad01b22e66874882e3a3c3094fcde
BillyJBryant <3013565+billyjbryant@users.noreply.github.com>
2023-05-10 14:50:14 -0700

    Adding Print Details to frontend

--- static/ankersrv.js
+++ static/ankersrv.js
@@ -14,7 +14,7 @@ $(function () {
     }
 
     /**
-     * Function to show modal when clicking reload services button
+     * Show modal when clicking reload services button
      */
     $("#reload").on("click", function () {
         content = $("#reload").data("message");
@@ -51,11 +51,104 @@ $(function () {
     });
 
     /**
+     * Get temperature from input
+     * @param {number} temp Temperature in Celsius
+     * @returns {number} Rounded temperature
+     */
+    function getTemp(temp) {
+        return Math.round(temp / 100);
+    }
+
+    /**
+     * Calculate the percentage between two numbers
+     * @param {number} layer
+     * @param {number} total
+     * @returns {number} percentage
+     */
+    function getPercentage(layer, total) {
+        return Math.round((layer / total) * 100);
+    }
+
+    /**
+     * Truncate a string to a specific length
+     * @param {string} str
+     * @param {number} maxLength
+     * @returns {string} Truncated string
+     */
+    function truncStr(str, maxLength) {
+        if (str.length > maxLength) {
+            return str.slice(0, maxLength) + "...";
+        } else {
+            return str;
+        }
+    }
+
+    /**
+     * Convert time in seconds to hours, minutes, and seconds format
+     * @param {number} seconds
+     * @returns {string} Formatted time string
+     */
+    function getTime(seconds) {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const remainingSeconds = seconds % 60;
+
+        const timeString = `${hours.toString().padStart(2, "0")}:${minutes
+            .toString()
+            .padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+
+        return timeString;
+    }
+
+    /**
+     * Get the filament weight based on its length
+     * @param {number} length
+     * @returns {number} Weight
+     */
+    function getWeight(length) {
+        const diameter = 1.75;
+        const density = 1.24;
+        const adjustedLength = length / 100;
+        console.log(adjustedLength);
+        const volume = Math.PI * (diameter / 2) ** 2 * adjustedLength;
+        const weight = (volume * density).toFixed(2);
+        return weight;
+    }
+
+    /**
      * Opens a websocket connection and outputs any incoming message data to console
      */
     socket = new WebSocket("ws://" + location.host + "/ws/mqtt");
     socket.addEventListener("message", (ev) => {
-        console.log(JSON.parse(ev.data));
+        const data = JSON.parse(ev.data);
+        if (data.commandType == 1001) {
+            // Returns Print Details
+            $("#print_active").attr("style", "display: block;");
+            $("#print_name").text(truncStr(data.name, 32));
+            $("#time_elapsed").text(getTime(data.totalTime));
+            $("#time_remain").text(getTime(data.time));
+            $("#filament").text(getWeight(data.filamentUsed));
+        } else if (data.commandType == 1003) {
+            // Returns Nozzle Temp
+            $("#noz_temp").text(getTemp(data.currentTemp));
+            $("#noz_target").text(getTemp(data.targetTemp));
+        } else if (data.commandType == 1004) {
+            // Returns Bed Temp
+            $("#bed_temp").text(getTemp(data.currentTemp));
+            $("#bed_target").text(getTemp(data.targetTemp));
+        } else if (data.commandType == 1052) {
+            // Returns Layer Info
+            $("#print_layer").text(data.real_print_layer);
+            $("#total_layer").text(data.total_layer);
+            const printPercent = getPercentage(data.real_print_layer, data.total_layer);
+            $("#progressbar").attr("aria-valuenow", printPercent);
+            $("#progressbar").attr("style", `width: ${printPercent}%`);
+            $("#progress").text(`${printPercent}%`);
+        } else if (data.commandType == 1006) {
+            // Returns Print Speed
+            $("#print_speed").text(data.value);
+        }
+        console.log(data);
     });
 
     /**
