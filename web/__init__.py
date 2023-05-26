2020ab8589d866272d8d1e48fb414f2903e76df8
Christian Iversen <ci@iversenit.dk>
2023-05-27 01:10:23 +0200

    * Move ankerctl api endpoints to web.api.ankerctl, implemented with a Blueprint

--- web/__init__.py
+++ web/__init__.py
@@ -142,80 +142,6 @@ def app_api_version():
     return {"api": "0.1", "server": "1.9.0", "text": "OctoPrint 1.9.0"}
 
 
-@app.post("/api/ankerctl/config/upload")
-def app_api_ankerctl_config_upload():
-    """
-    Handles the uploading of configuration file to Flask server
-
-    Returns:
-        A HTML redirect response
-    """
-    if request.method != "POST":
-        return web.util.flash_redirect(url_for('app_root'))
-    if "login_file" not in request.files:
-        return web.util.flash_redirect(url_for('app_root'), "No file found", "danger")
-    file = request.files["login_file"]
-
-    try:
-        web.config.config_import(file, app.config["config"])
-        return web.util.flash_redirect(url_for('app_api_ankerctl_server_reload'),
-                                       "AnkerMake Config Imported!", "success")
-    except web.config.ConfigImportError as err:
-        log.exception(f"Config import failed: {err}")
-        return web.util.flash_redirect(url_for('app_root'), f"Error: {err}", "danger")
-    except Exception as err:
-        log.exception(f"Config import failed: {err}")
-        return web.util.flash_redirect(url_for('app_root'), f"Unexpected Error occurred: {err}", "danger")
-
-
-@app.get("/api/ankerctl/server/reload")
-def app_api_ankerctl_server_reload():
-    """
-    Reloads the Flask server
-
-    Returns:
-        A HTML redirect response
-    """
-    config = app.config["config"]
-
-    with config.open() as cfg:
-        app.config["login"] = bool(cfg)
-        if not cfg:
-            return web.util.flash_redirect(url_for('app_root'), "No printers found in config", "warning")
-        if "_flashes" in session:
-            session["_flashes"].clear()
-
-        try:
-            app.svc.restart_all(await_ready=False)
-        except Exception as err:
-            log.exception(err)
-            return web.util.flash_redirect(url_for('app_root'), f"Ankerctl could not be reloaded: {err}", "danger")
-
-        return web.util.flash_redirect(url_for('app_root'), "Ankerctl reloaded successfully", "success")
-
-
-@app.post("/api/ankerctl/file/upload")
-def app_api_ankerctl_file_upload():
-    if request.method != "POST":
-        return web.util.flash_redirect(url_for('app_root'))
-    if "gcode_file" not in request.files:
-        return web.util.flash_redirect(url_for('app_root'), "No file found", "danger")
-    file = request.files["gcode_file"]
-
-    try:
-        web.util.upload_file_to_printer(app, file)
-        return web.util.flash_redirect(url_for('app_root'),
-                                       f"File {file.filename} sent to printer!", "success")
-    except ConnectionError as err:
-        return web.util.flash_redirect(url_for('app_root'),
-                                       "Cannot connect to printer!\n"
-                                       "Please verify that printer is online, and on the same network as ankerctl.\n"
-                                       f"Exception information: {err}", "danger")
-    except Exception as err:
-        return web.util.flash_redirect(url_for('app_root'),
-                                       f"Unknown error occurred: {err}", "danger")
-    
-    
 @app.post("/api/files/local")
 def app_api_files_local():
     """
@@ -264,6 +190,7 @@ def webserver(config, printer_index, host, port, insecure=False, **kwargs):
     """
     with config.open() as cfg:
         import web.api.ws
+        import web.api.ankerctl
 
         if cfg and printer_index >= len(cfg.printers):
             log.critical(f"Printer number {printer_index} out of range, max printer number is {len(cfg.printers)-1} ")
@@ -284,5 +211,6 @@ def webserver(config, printer_index, host, port, insecure=False, **kwargs):
         app.hotbed_target = 0.0
 
         app.register_blueprint(web.api.ws.router, url_prefix="/ws")
+        app.register_blueprint(web.api.ankerctl.router, url_prefix="/api/ankerctl")
 
         app.run(host=host, port=port)
