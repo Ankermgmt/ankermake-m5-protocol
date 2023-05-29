9bf05a66167eee6f6d9286dfe9c7154488cb6006
Christian Iversen <ci@iversenit.dk>
2023-05-29 23:26:30 +0200

    + Added minimal implementation of server.job_queue.{status, post_job, delete_job}

--- web/moonraker/rpc/server/job_queue.py
+++ web/moonraker/rpc/server/job_queue.py
@@ -1,36 +1,40 @@
 from jsonrpc import dispatcher
 from flask import current_app as app
 
+from libflagship.util import unhex
+
 
 @dispatcher.add_method(name="server.job_queue.status")
 def server_job_queue_status():
-    return {
-        "queued_jobs": [],
-        "queue_state": "ready",
-    }
+    with app.svc.borrow("jobqueue") as jq:
+        return jq.queue_state()
 
 
 @dispatcher.add_method(name="server.job_queue.post_job")
 def server_job_queue_post_job(filenames, reset=False):
-    d = {
-        "queued_jobs": [
-            {
-             "filename": filenames[0],
-             "job_id": "0000000066D99C90",
-             "time_added": 1636151050.7666452,
-             "time_in_queue": 21.89680004119873
-            }
-        ],
-        "queue_state": "ready",
-    }
-    with app.svc.borrow("updates") as upd:
-        upd.notify_job_queue_changed("jobs_added", d["queued_jobs"], d["queue_state"])
-    return d
+    with app.svc.borrow("jobqueue") as jq:
+        if reset:
+            jq.queue.jobs.clear()
+
+        for filename in filenames:
+            jq.post_job(filename)
+
+        with app.svc.borrow("updates") as upd:
+            upd.notify_job_queue_changed("jobs_added", jq.queued_jobs(), "ready")
+
+        return jq.queue_state()
 
 
 @dispatcher.add_method(name="server.job_queue.delete_job")
 def server_job_queue_delete_job(job_ids):
-    ...
+    with app.svc.borrow("jobqueue") as jq:
+        job_ids = [unhex(ji) for ji in job_ids]
+        jq.delete_jobs(job_ids)
+
+        with app.svc.borrow("updates") as upd:
+            upd.notify_job_queue_changed("jobs_removed", jq.queued_jobs(), "ready")
+
+        return jq.queue_state()
 
 
 @dispatcher.add_method(name="server.job_queue.pause")
