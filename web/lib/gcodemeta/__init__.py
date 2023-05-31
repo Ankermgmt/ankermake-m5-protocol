4a652e005e4924c6f677a93c00b647ca8c72204d
Christian Iversen <ci@iversenit.dk>
2023-05-31 18:10:39 +0200

    + Add GCodeMetaAuto class for auto-detecting gcode metadata, based on provided detector classes

--- web/lib/gcodemeta/__init__.py
+++ web/lib/gcodemeta/__init__.py
@@ -10,3 +10,24 @@ class GCodeMeta:
 
     def load_props(self, fd):
         raise NotImplemented
+
+
+class GCodeMetaAuto(GCodeMeta):
+
+    def __init__(self, types):
+        self.types = types
+
+    def detect(self, fd):
+        for t in self.types:
+            if res := t.detect(fd):
+                self.type = t
+                return res
+
+    def detect_first_line(self, line):
+        for t in self.types:
+            if res := t.detect(fd):
+                self.type = t
+                return res
+
+    def load_props(self, fd):
+        return self.type.load_props(fd)
