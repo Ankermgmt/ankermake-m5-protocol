62fee19ed37971a1680bde1a632608409966e61e
Christian Iversen <ci@iversenit.dk>
2023-05-31 18:12:03 +0200

    + Implement support for parsing metadata in head of ankerslicer gcode

--- web/lib/gcodemeta/ankerslicer.py
+++ web/lib/gcodemeta/ankerslicer.py
@@ -17,14 +17,25 @@ class GCodeMetaAnkerSlicer(GCodeMeta):
         except json.decoder.JSONDecodeError:
             return val
 
-    def load_props(self, fd):
-        rsize = 128 * 1024
-        fd.seek(0, 2)
-        fsize = fd.tell()
-        size = min(fsize, rsize)
-        fd.seek(-size, 2)
-        data = fd.read(size)
+    def _parse_head(self, data):
+        res = {}
 
+        for line in data.splitlines():
+            line = line.decode()
+
+            if not line.startswith(";"):
+                continue
+
+            if ":" not in line:
+                continue
+
+            key, value = line.split(":", 1)
+            key = key[1:].lower().replace(" ", "_")
+            res[f"_{key}"] = self.parse_prop(value.strip())
+
+        return res
+
+    def _parse_tail(self, data):
         if not b";paramBegin" in data:
             return {}
 
@@ -46,5 +57,31 @@ class GCodeMetaAnkerSlicer(GCodeMeta):
 
             key, value = line.split("=", 1)
             res[key] = self.parse_prop(value)
+
+        return res
+
+    def load_props(self, fd):
+        headsize = 32 * 1024
+        tailsize = 32 * 1024
+
+        fd.seek(0, 2)
+        fsize = fd.tell()
+
+        if fsize > (headsize + tailsize):
+            fd.seek(-tailsize, 2)
+            tail = fd.read(tailsize)
+            fd.seek(0)
+            head = fd.read(headsize)
+            return {
+                **self._parse_head(head),
+                **self._parse_tail(tail),
+            }
+        else:
+            fd.seek(0)
+            data = fd.read()
+            return {
+                **self._parse_head(data),
+                **self._parse_tail(data),
+            }
 
         return res
