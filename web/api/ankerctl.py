5fc468a934499325bdbacceb32da2ab3d3d750c9
Christian Iversen <ci@iversenit.dk>
2023-06-01 18:31:53 +0200

    * Fix redirects in web/api/ankerctl.py, which should now point to base.app_root (reworked patch from @billyjbryant)

--- web/api/ankerctl.py
+++ web/api/ankerctl.py
@@ -19,9 +19,9 @@ def app_api_ankerctl_config_upload():
         A HTML redirect response
     """
     if request.method != "POST":
-        return web.util.flash_redirect(url_for('app_root'))
+        return web.util.flash_redirect_root()
     if "login_file" not in request.files:
-        return web.util.flash_redirect(url_for('app_root'), "No file found", "danger")
+        return web.util.flash_redirect_root("No file found", "danger")
     file = request.files["login_file"]
 
     try:
@@ -30,10 +30,10 @@ def app_api_ankerctl_config_upload():
                                        "AnkerMake Config Imported!", "success")
     except web.config.ConfigImportError as err:
         log.exception(f"Config import failed: {err}")
-        return web.util.flash_redirect(url_for('app_root'), f"Error: {err}", "danger")
+        return web.util.flash_redirect_root(f"Error: {err}", "danger")
     except Exception as err:
         log.exception(f"Config import failed: {err}")
-        return web.util.flash_redirect(url_for('app_root'), f"Unexpected Error occurred: {err}", "danger")
+        return web.util.flash_redirect_root(f"Unexpected Error occurred: {err}", "danger")
 
 
 @router.get("/server/reload")
@@ -49,7 +49,7 @@ def app_api_ankerctl_server_reload():
     with config.open() as cfg:
         app.config["login"] = bool(cfg)
         if not cfg:
-            return web.util.flash_redirect(url_for('app_root'), "No printers found in config", "warning")
+            return web.util.flash_redirect_root("No printers found in config", "warning")
         if "_flashes" in session:
             session["_flashes"].clear()
 
@@ -57,28 +57,28 @@ def app_api_ankerctl_server_reload():
             app.svc.restart_all(await_ready=False)
         except Exception as err:
             log.exception(err)
-            return web.util.flash_redirect(url_for('app_root'), f"Ankerctl could not be reloaded: {err}", "danger")
+            return web.util.flash_redirect_root(f"Ankerctl could not be reloaded: {err}", "danger")
 
-        return web.util.flash_redirect(url_for('app_root'), "Ankerctl reloaded successfully", "success")
+        return web.util.flash_redirect_root("Ankerctl reloaded successfully", "success")
 
 
 @router.post("/file/upload")
 def app_api_ankerctl_file_upload():
     if request.method != "POST":
-        return web.util.flash_redirect(url_for('app_root'))
+        return web.util.flash_redirect_root()
     if "gcode_file" not in request.files:
-        return web.util.flash_redirect(url_for('app_root'), "No file found", "danger")
+        return web.util.flash_redirect_root("No file found", "danger")
     file = request.files["gcode_file"]
 
     try:
         web.util.upload_file_to_printer(app, file)
-        return web.util.flash_redirect(url_for('app_root'),
-                                       f"File {file.filename} sent to printer!", "success")
+        return web.util.flash_redirect_root(f"File {file.filename} sent to printer!", "success")
     except ConnectionError as err:
-        return web.util.flash_redirect(url_for('app_root'),
-                                       "Cannot connect to printer!\n"
-                                       "Please verify that printer is online, and on the same network as ankerctl.\n"
-                                       f"Exception information: {err}", "danger")
+        return web.util.flash_redirect_root(
+            "Cannot connect to printer!\n"
+            "Please verify that printer is online, and on the same network as ankerctl.\n"
+            f"Exception information: {err}",
+            "danger"
+        )
     except Exception as err:
-        return web.util.flash_redirect(url_for('app_root'),
-                                       f"Unknown error occurred: {err}", "danger")
+        return web.util.flash_redirect_root(f"Unknown error occurred: {err}", "danger")
