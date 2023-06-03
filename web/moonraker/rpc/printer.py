efdbfb1b757f2dc256b194efeda0763f712a5c9e
Christian Iversen <ci@iversenit.dk>
2023-06-04 01:49:56 +0200

    * Use UpdateManager to simplify printer.objects.subscribe rpc endpoint (and actually provide correct results)

--- web/moonraker/rpc/printer.py
+++ web/moonraker/rpc/printer.py
@@ -1,6 +1,7 @@
 import time
 import psutil
 
+from datetime import datetime
 from flask import current_app as app
 from jsonrpc import dispatcher
 
@@ -106,90 +107,12 @@ def printer_objects_list():
 @dispatcher.add_method(name="printer.objects.subscribe")
 def printer_objects_subscribe(objects):
     with app.svc.borrow("updates") as upd:
-        pstate = upd.pstate
+        status = upd.umgr.moonraker_status_full()
 
     return {
-        "eventtime": 3793707.88993766,
+        "eventtime": datetime.now().timestamp(),
         "status": {
-            "webhooks": {
-                "state": "ready",
-                "state_message": "Printer is ready"
-            },
-            "print_stats": {
-                "filename": "",
-                "total_duration": 0,
-                "print_duration": 0,
-                "filament_used": 0,
-                "state": "standby",
-                "message": "",
-                "info": {
-                    "total_layer": None,
-                    "current_layer": None
-                }
-            },
-            "heater_bed": {
-                "temperature": pstate.hotbed.current,
-                "target": pstate.hotbed.target,
-                "power": 0,
-            },
-            "extruder": {
-                "temperature": pstate.nozzle.current,
-                "target": pstate.nozzle.target,
-                "power": 0,
-                "can_extrude": True,
-                "pressure_advance": 0.1,
-                "smooth_time": 0.01,
-                "motion_queue": None,
-            },
-            "heaters": {
-                "available_heaters": [
-                    "heater_bed",
-                    "extruder"
-                ],
-                "available_sensors": [
-                    "heater_bed",
-                    "extruder"
-                ]
-            },
-            "display_status": {
-                "progress": 0,
-                "message": "Greetings, Professor Falken"
-            },
-            "idle_timeout": {
-                "state": "Idle",
-                "printing_time": 0
-            },
-            "toolhead": {
-                "homed_axes": "",
-                "axis_minimum": [
-                    0,
-                    0,
-                    0,
-                    0
-                ],
-                "axis_maximum": [
-                    235,
-                    235,
-                    250,
-                    0
-                ],
-                "print_time": None,
-                "stalls": 0,
-                "estimated_print_time": None,
-                "extruder": "extruder",
-                "position": [
-                    0,
-                    0,
-                    0,
-                    0
-                ],
-                "max_velocity": 600,
-                "max_accel": 6000,
-                "max_accel_to_decel": None,
-
-                # 15 / sqrt(2)
-                "square_corner_velocity": 10.6
-            },
+            **status,
             "configfile": {
                 "config": {
                     "timelapse": {},
@@ -333,12 +256,6 @@ def printer_objects_subscribe(objects):
                 "save_config_pending": False,
                 "save_config_pending_items": {}
             },
-            "mcu": {
-                "mcu_version": "Linux 4.4.94",
-                "mcu_build_versions": "Ingenic r4.1.1-gcc720-glibc226-fp64 2020.11-05",
-                "mcu_constants": {},
-                "last_stats": {},
-            },
             "gcode_macro START_PRINT": {},
             "gcode_macro END_PRINT": {},
             "gcode_macro CANCEL_PRINT": {},
@@ -354,105 +271,6 @@ def printer_objects_subscribe(objects):
             "gcode_macro M190": {},
             "gcode_macro ANKERMAKE_LIGHT_ON": {},
             "gcode_macro ANKERMAKE_LIGHT_OFF": {},
-            "stepper_enable": {
-                "steppers": {
-                    "stepper_x": False,
-                    "stepper_y": False,
-                    "stepper_z": False,
-                    "extruder": False
-                }
-            },
-            "gcode_move": {
-                "speed_factor": 1,
-                "speed": 1500,
-                "extrude_factor": 1,
-                "absolute_coordinates": True,
-                "absolute_extrude": True,
-                "homing_origin": [
-                    0,
-                    0,
-                    0,
-                    0
-                ],
-                "position": [
-                    0,
-                    0,
-                    0,
-                    0
-                ],
-                "gcode_position": [
-                    0,
-                    0,
-                    0,
-                    0
-                ]
-            },
-            "exclude_object": {
-                "objects": [],
-                "excluded_objects": [],
-                "current_object": None
-            },
-            "bed_mesh": {
-                "profile_name": "",
-                "mesh_min": [
-                    0,
-                    0
-                ],
-                "mesh_max": [
-                    0,
-                    0
-                ],
-                "probed_matrix": [
-                    []
-                ],
-                "mesh_matrix": [
-                    []
-                ],
-                "profiles": {
-                    "default": {
-                        "points": [
-                            [0.075, 0.069792, 0.062708, 0.044167, 0.032917],
-                            [0.045417, 0.043958, 0.066042, 0.053333, 0.03875],
-                            [0.034375, 0.023333, 0.032917, 0.054167, 0.043542],
-                            [0.076042, 0.056458, 0.06625, 0.045, 0.057292],
-                            [0.051875, 0.036875, 0.03, 0.020625, 0.043125]
-                        ],
-                        "mesh_params": {
-                            "min_x": 10,
-                            "max_x": 225,
-                            "min_y": 10,
-                            "max_y": 225,
-                            "x_count": 5,
-                            "y_count": 5,
-                            "mesh_x_pps": 5,
-                            "mesh_y_pps": 5,
-                            "algo": "bicubic",
-                            "tension": 0.2
-                        }
-                    },
-                    "mesh_profile_2": {
-                        "points": [
-                            [0.025, 0.029792, 0.022708, 0.024167, 0.022917],
-                            [0.025417, 0.023958, 0.026042, 0.023333, 0.02875],
-                            [0.024375, 0.023333, 0.022917, 0.024167, 0.023542],
-                            [0.026042, 0.026458, 0.02625, 0.025, 0.027292],
-                            [0.021875, 0.026875, 0.02, 0.020625, 0.02312]
-                        ],
-                        "mesh_params": {
-                            "min_x": 10,
-                            "max_x": 225,
-                            "min_y": 10,
-                            "max_y": 225,
-                            "x_count": 5,
-                            "y_count": 5,
-                            "mesh_x_pps": 5,
-                            "mesh_y_pps": 5,
-                            "algo": "bicubic",
-                            "tension": 0.2
-                        }
-                    },
-                }
-            },
             "system_stats": {
                 "sysload": psutil.getloadavg()[0],
                 "cputime": psutil.cpu_times().user,
