b809cb2405c120723a1bf36618512c0cf3f6ef6e
Christian Iversen <ci@iversenit.dk>
2023-06-03 17:43:58 +0200

    + Stubbed out machine.device_power.post_device

--- web/moonraker/rpc/machine/device_power.py
+++ web/moonraker/rpc/machine/device_power.py
@@ -11,3 +11,8 @@ def machine_device_power_devices():
 @dispatcher.add_method(name="machine.device_power.on")
 def machine_device_power_on(**objs):
     return "ok"
+
+
+@dispatcher.add_method(name="machine.device_power.post_device")
+def machine_device_power_power_device(**objs):
+    raise NotImplementedError()
