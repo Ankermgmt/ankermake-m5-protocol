89db2197b225ffabd1be2454b61b35d72a89e9f0
Christian Iversen <ci@iversenit.dk>
2023-06-05 11:20:21 +0200

    + Implement .load_metadata() and .load_metadata_dict() on JobQueueService

--- web/service/jobqueue.py
+++ web/service/jobqueue.py
@@ -1,9 +1,15 @@
+import uuid
+from pathlib import Path
 from datetime import datetime
 from platformdirs import PlatformDirs
 
 from cli.config import BaseConfigManager
 from ..lib.service import Service
 from web.model import Job, JobQueue, FileMetadata
+from web.lib.gcodemeta import GCodeMetaAuto
+from web.lib.gcodemeta.ankerslicer import GCodeMetaAnkerSlicer
+from web.lib.gcodemeta.prusaslicer import GCodeMetaPrusaSlicer
+from web.lib.gcodemeta.superslicer import GCodeMetaSuperSlicer
 
 
 class JobQueueService(Service):
@@ -14,6 +20,11 @@ class JobQueueService(Service):
             self.queue = self.cfg.load("jobs", JobQueue(jobs=[], history=[]))
         except OSError:
             pass
+        self.gcode_loader = GCodeMetaAuto([
+            GCodeMetaAnkerSlicer(),
+            GCodeMetaPrusaSlicer(),
+            GCodeMetaSuperSlicer(),
+        ])
 
     def worker_start(self):
         self.upd = self.app.svc.get("updates", ready=False)
@@ -25,6 +36,32 @@ class JobQueueService(Service):
         self.cfg.save("jobs", self.queue)
 
         self.app.svc.put("updates")
+
+    def load_metadata(self, path: Path):
+        stat = path.stat()
+        fd = path.open("rb")
+
+        gcm = self.gcode_loader
+        if gcm.detect(fd):
+            props = gcm.load_props(fd)
+            md = gcm.load_metadata(props)
+        else:
+            md = FileMetadata()
+
+        md.size = stat.st_size
+        md.modified = stat.st_mtime
+        md.filename = str(path)
+        return md
+
+    def load_metadata_dict(self, path: Path):
+        md = self.load_metadata(path).to_dict()
+        for h in self.queue.history:
+            if h.filename == path:
+                md["job_id"] = h.job_id
+                md["uuid"] = h.metadata.uuid
+                break
+
+        return md
 
     def queued_jobs(self):
         return [j.to_dict() for j in self.queue.jobs]
