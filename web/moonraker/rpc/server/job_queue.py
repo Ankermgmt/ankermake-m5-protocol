0fb2bbfd6dec06d527f6d94e688cba701e9714fe
Christian Iversen <ci@iversenit.dk>
2023-06-05 11:25:25 +0200

    + Implement proof-of-concept server.job_queue.start. Not the correct behavior, but better than nothing.

--- web/moonraker/rpc/server/job_queue.py
+++ web/moonraker/rpc/server/job_queue.py
@@ -1,5 +1,8 @@
+import logging as log
+
 from jsonrpc import dispatcher
 from flask import current_app as app
+from pathlib import Path
 
 from libflagship.util import unhex
 
@@ -37,7 +40,21 @@ def server_job_queue_pause():
 
 @dispatcher.add_method(name="server.job_queue.start")
 def server_job_queue_start():
-    ...
+    with app.svc.borrow("jobqueue") as jq:
+        queue = jq.queue
+        job = queue.jobs[0]
+
+        gcodes = Path("database/gcodes")
+        jobfile = gcodes / job.filename
+        with app.svc.borrow("filetransfer") as ft:
+            fd = jobfile.open("rb")
+            fd.filename = job.filename
+            try:
+                ft.send_file(fd, user_name="ankerctl")
+            except Exception as e:
+                log.exception(f"Failed to start queue: {e}")
+
+    return "ok"
 
 
 @dispatcher.add_method(name="server.job_queue.jump")
