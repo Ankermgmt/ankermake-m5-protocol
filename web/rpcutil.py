dddd8ac3cbd7d7d8dd3e5861282b42ca08cf4618
Christian Iversen <ci@iversenit.dk>
2023-06-11 21:54:59 +0200

    * Convert rpcutil to use instance logger, and make rpc response output less verbose

--- web/rpcutil.py
+++ web/rpcutil.py
@@ -1,5 +1,7 @@
 import json
-import logging as log
+import logging
+
+log = logging.getLogger("RPC")
 
 
 def format_jsonrpc_req(req):
@@ -10,14 +12,25 @@ def format_jsonrpc_req(req):
     return f"{req['method']}({', '.join(args)})"
 
 
+def format_jsonrpc_resp(resp):
+    if isinstance(resp, dict):
+        keys = ", ".join(repr(k) for k in resp.keys())
+        return f"{{{keys}}}"
+    elif isinstance(resp, list):
+        keys = ", ".join(resp)
+        return f"[{keys}]"
+    else:
+        return repr(resp)
+
+
 def log_jsonrpc_req(req, response):
     try:
         if response.error:
-            log.error(f"[RPC] {format_jsonrpc_req(req)} = {json.dumps(response.error, indent=4)}")
+            log.error(f"{format_jsonrpc_req(req)} = {json.dumps(response.error, indent=4)}")
         else:
-            log.info(f"[RPC] {format_jsonrpc_req(req)} = {json.dumps(response.result)}")
+            log.info(f"{format_jsonrpc_req(req)} = {format_jsonrpc_resp(response.result)}")
     except TypeError:
-        log.error(f"[RPC] {format_jsonrpc_req(req)} INVALID JSON VALUE: {response.result}")
+        log.error(f"{format_jsonrpc_req(req)} INVALID JSON VALUE: {response.result}")
 
 
 def make_jsonrpc_req(method, *args, **kwargs):
